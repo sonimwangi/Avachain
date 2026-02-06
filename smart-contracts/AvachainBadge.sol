14af72d726ee01f114c17a18b63afae3e20f6ba8
sonimwangi <162610347+sonimwangi@users.noreply.github.com>
2026-02-06 17:16:18 +0300

    Update AvachainBadge.sol file

--- smart-contracts/AvachainBadge.sol
+++ smart-contracts/AvachainBadge.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AvachainBadge is ERC721, Ownable {
+    uint256 public nextTokenId;
+
+    constructor() ERC721("Avachain Badge", "ACB") {}
+
+    function mintBadge(address to) external onlyOwner {
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+}
